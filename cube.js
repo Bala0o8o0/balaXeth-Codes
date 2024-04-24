a9b68ca413189a754669f7f23275f3661ff19e79
balaXeth <118262493+Bala0o8o0@users.noreply.github.com>
2024-04-24 21:02:35 +0530

    Delete cube.js

--- cube.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".nft-left-overlay").hover(function () {
-    $(this).parents(".nft-cube").toggleClass("left");
-  });
-
-  $(".nft-right-overlay").hover(function () {
-    $(this).parents(".nft-cube").toggleClass("right");
-  });
-});
